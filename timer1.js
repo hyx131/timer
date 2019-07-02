736f8fa8d314ebc2dd7d2e4e0d25c7808303d31d
Selin Huang <selin.hyx@gmail.com>
2019-07-02 17:12:36 +0000

    created timer function with programming pair Kendall

---
+++ timer1.js
@@ -0,0 +1,16 @@
+let arr = process.argv.slice(2);
+
+
+
+
+const timer = function(arr) {
+  for (let item of arr) {
+    if (item > 0) {
+      setTimeout(() => process.stdout.write(item), parseInt(item) * 1000);
+    }
+  }
+};
+
+timer(arr);
+
+
